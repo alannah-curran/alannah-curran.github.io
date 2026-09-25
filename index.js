483273e53441606e51adf5fbdf1d5227af11c230
alannah-curran <alannahc@bu.edu>
2026-09-25 01:14:16 -0400

    added new page and image zoom capabilities

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log('Happy developing ✨')
+
+function zoom(img) {
+    document.getElementById('zoom').src = img;
+    document.getElementById('zoomed-in').style.display = 'flex';
+}
+function closeZoomed() {
+    document.getElementById('zoomed-in').style.display = 'none';
+}
